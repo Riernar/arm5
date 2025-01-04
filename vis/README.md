cf60d93b395566e0f01d8e7e5e43b5324731c9c2
Riernar <Riernar@users.noreply.github.com>
2025-01-05 00:02:58 +0100

    feat(vis): add vis ledger script

---
+++ vis/README.md
@@ -0,0 +1,120 @@
+# Vis script
+
+Script to manage vis stock in a covenant. From a list of vis transaction (e.g. vis sources, ritual castings,
+enchantments, ...) produces a ledger that describe the state of the vis stocks for all years and the consumption of each
+magi, as well a detailed records of vis movement.
+
+## Installation
+
+The script is a `python>=3.12` script. We recommend using [`uv`][uv] to run the script, as `uv run --script vis.py` will
+automatically manage python version and the dependencies needed to run the script.
+
+The script has a shebang using `uv` to run it so that execution does not require managing a python virtual environment
+and dependencies manually.
+
+1. Install [`uv`][uv]
+2. Download and copy the script somewhere
+3. Depending on your operating system:
+   - **Unix** (Linux, MacOS)
+     1. Make the script executable with `chmod u+x vis.py`
+     2. Run the script directly, e.g. `./vis.py --help`
+   - **Windows** Windows does not understand shebang, so you'll have to invoke `uv` manually every time:
+     `uv run --script vis.py`
+
+The script can also be run directly with python >=3.12 with `python vis.py`. You are responsible for installing any
+dependencies defined in the script [inline metadata][py-inline-metadata].
+
+[uv]: https://docs.astral.sh/uv/
+[py-inline-metadata]: https://packaging.python.org/en/latest/specifications/inline-script-metadata/
+
+## Usage
+
+The script follows POSIX semantics and supports reading from STDIN and writing to STDOUT/STDERR. Messages for the user
+(e.g. logs) are written to STDERR, while machine-readable output (e.g. JSON) are written to STDOUT.
+
+The `-i` and `-o` command-line arguments can be used to specify files to read from/write to. If `-i` and/or `-o` are not
+passed, the script will by default read/write from/to STDIN/STDOUT. By default, the scripts reads and write:
+
+- JSON if `-i` and/or `-o` are not provided
+- else the format is determined from the file extension(s).
+
+The `-input-format` and `--output-format` argument can be used to control the formats.
+
+See also the `--help` message of the script for detailed CLI arguments.
+
+### Inputs
+
+The inputs processed by the script are:
+
+- A list of transactions, that is, changes to the stocks
+- An optional starting stock
+
+The supported input formats are either JSON or CSV.
+
+#### JSON Inputs (default)
+
+The input must be a JSON object with the following [cuelang][cuelang] schema:
+
+```cuelang
+records!: [#VisRecord]
+start_stock?: #Vis
+
+#VisRecord: {
+    description: string
+    vis: #Vis
+    year: int
+    season: "spring" | "summer" | "autumn" | "winter"
+    magus?: string
+    period?: int
+    end_year?: int
+}
+
+#Vis: [string]: int
+
+```
+
+where:
+
+- `records`: an array of transaction records, which describe input or output of vis in the stocks, like in a ledger. A
+  record is made of:
+  - `description`: A description of the event, such as a vis source name, the enchantment being made with the consumed
+    vis, the ritual casted, ...
+  - `vis`: What kind of vis is changed by what amount, e.g. +3He or -5 Vim. This is a mapping from Vis name to diff (so
+    positive adds to the stock, negative removes from the stock). Vis name are just strings and the script will not
+    unify abbreviated names (e.g. Im) with full names (e.g. Imaginem).
+    - There is special handling for dedicated vis. You can use any of the `.,;:/|+&-` character as a separator, and this
+      will be treated as dedicated vis (this affect sorting order of columns in markdown output). E.g. `Cr/Co` is Creo
+      Corpus dedicated vis.
+    - A single record can modify several type of vis at the same time since this entry is a JSON object from vis name to
+      the amount change.
+  - `year`: The year the record affects the stocks
+  - `season`: The season the record affect the stocks. Must be one of `spring`, `summer`, `autumn`, `winter`
+  - `magus` (optional): Name of the magus that brought or took the Vis from the stock
+  - `period` (optional): For repeating records (e.g. an annual vis source, the _Aegis of the Hearth_ casting), the
+    period in years of the repetitions. E.g. 1 for a modification to apply each year, 2 to apply every other year,
+    etc...
+  - `end_year` (optional): If `period` is define, the repeating input/output stops past this year
+- `start_stock` (optional): The starting stock of vis available at the start of the accounting. Again, a JSON object
+  mapping vis names to the starting amount.
+
+[cuelang]: https://cuelang.org/docs/tour/
+
+#### CSV Inputs
+
+The CSV inputs is a list of record, one per row, with the same properties as in the JSON inputs. As CSV cannot support
+the starting stock and several vis on the same row:
+
+- the `vis` property is split into two columns, `art` and `amount`. The art of the vis (a string) must be written in the
+  art column, and the amount of change in the `amount` column
+- Rows which are identical save for the `art` and `amount` columns are aggregated together to form as single record,
+  each line effectively specifying one entry in the `vis` mapping.
+- If the `year` column is empty, the `art` and `amount` are added to the starting stock, and all other columns ignored
+
+### Outputs
+
+The script produces a ledger with detailed vis stocks as a function of time (year and season), total consumption for
+each magi and detailed records of what happened to the stocks for each year and season.
+
+The default output format is JSON, which contains the full details. The script can also produce a markdown report that
+is much more human readable, featuring tables. In this case, the main stock tables only describe the state of the stock
+per year, instead of per season, to keep it readable.
---
+++ vis/vis.py
@@ -0,0 +1,527 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+# ]
+# ///
+
+import argparse
+import collections
+import collections.abc
+import contextlib
+import csv
+import dataclasses
+import enum
+import functools
+import json
+import logging
+import operator
+import pathlib
+import re
+import sys
+import typing
+from typing import Any, Final, Iterable, Iterator, Literal, Mapping, Sequence, TextIO
+
+LOGGER = logging.getLogger(__name__)
+
+
+class UserError(Exception):
+    """Known error that should be reported to the user"""
+
+
+def cli() -> None:
+    parser = argparse.ArgumentParser(description="CLI program to manage vis stocks")
+
+    inputs = parser.add_argument_group(title="inputs", description="inputs")
+    inputs.add_argument(
+        "-i",
+        "--input",
+        default=sys.stdin,
+        type=pathlib.Path,
+        help="Input file to read input data from. Defaults to STDIN.",
+    )
+    inputs.add_argument(
+        "--input-format",
+        choices=["json", "csv"],
+        help=(
+            "Input format. Defaults to 'json' if reading from STDIN,"
+            " or detected from input file extension if reading from a file."
+        ),
+    )
+
+    outputs = parser.add_argument_group(title="outputs", description="outputs")
+    outputs.add_argument(
+        "-o", "--output", default=sys.stdout, type=pathlib.Path, help="Output path. Defaults to STDOUT"
+    )
+    outputs.add_argument(
+        "--output-format",
+        choices=["json", "md"],
+        help=(
+            "Input format. Defaults to 'json' if writing to STDOUT,"
+            " or inferred from output file extension if writing to file."
+        ),
+    )
+
+    parser.add_argument("--debug", action="store_true", help="Activate debugging")
+    args = parser.parse_args()
+    _setup_logging()
+    try:
+        # Validate CLI args
+        input_format = getattr(args, "input_format", None) or _infer_format(
+            args.input, default="json", supported={"json", "csv"}
+        )
+        output_format = getattr(args, "output_format", None) or _infer_format(
+            args.output, default="json", supported={"json", "md"}
+        )
+
+        with (
+            _open_file_or_stream(args.input, mode="r") as infile,
+            _open_file_or_stream(args.output, mode="w") as outfile,
+        ):
+            # Notify the user to avoid confusion
+            if infile is sys.stdin:
+                LOGGER.info("Reading from STDIN")
+            start_stock, records = _READERS[input_format](infile)
+            ledger = VisLedger.from_records(records, start_stock=start_stock)
+            _WRITERS[output_format](ledger, outfile)
+
+    except UserError as err:
+        if sys.stdout.isatty():
+            template = "\033[1;91m{}\033[0m"
+        else:
+            template = "{}"
+        print(template.format(str(err)), file=sys.stderr, flush=True)
+        sys.exit(1)
+
+
+def _setup_logging() -> None:
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+
+def _infer_format(arg: TextIO | pathlib.Path, *, default: str, supported: set[str]) -> str:
+    if isinstance(arg, pathlib.Path):
+        # The slice [1:] removes the leading dot that is included in the suffix by python
+        extension = arg.suffixes[-1][1:]
+        if extension not in supported:
+            raise UserError(f"Unsupported format '{extension}' in {arg}: supported formats are {sorted(supported)}")
+        return extension
+
+    return default
+
+
+@contextlib.contextmanager
+def _open_file_or_stream(arg: TextIO | pathlib.Path, mode: Literal["r", "w"]) -> Iterator[TextIO]:
+    if isinstance(arg, pathlib.Path):
+        try:
+            with open(arg, mode, encoding="utf-8") as file:
+                yield file
+        except (FileNotFoundError, NotADirectoryError) as err:
+            raise UserError(f"{type(err).__name__}: {err}")
+
+    else:
+        yield arg
+
+
+def _read_json(stream: TextIO) -> tuple[Mapping[str, int], tuple["VisRecord", ...]]:
+    raw = json.load(stream)
+    if not isinstance(raw, dict):
+        raise ValueError(f"Expected top-level JSON Object, got {type(raw)}")
+    start_stock = {vis: int(quantity) for vis, quantity in raw.get("start_stock", {})}
+    records = tuple(VisRecord(**data) for data in raw.get("records", []))
+    return start_stock, records
+
+
+def _read_csv(stream: TextIO) -> tuple[Mapping[str, int], tuple["VisRecord", ...]]:
+    reader = csv.DictReader(stream)
+
+    # Build the fields we need to have in the CSV
+    fields = {field.name for field in dataclasses.fields(VisRecord)} - {"vis"}
+    key_fields = sorted(fields)
+    fields |= {"art", "amount"}
+
+    headers = set(reader.fieldnames)  # type: ignore
+    if unknown_headers := headers - fields:
+        raise UserError(f"Invalid CSV: unknown headers {sorted(unknown_headers)}: supported headers: {sorted(fields)}")
+    if missing_headers := fields - headers:
+        raise UserError(f"Invalid CSV:missing headers {sorted(missing_headers)}")
+
+    # When reading CSV, we need to agglomerate together lines that express several vis types for a single action
+    start_stock = collections.Counter()
+    groups = collections.defaultdict(collections.Counter)
+    get_key = operator.itemgetter(*key_fields)
+    for row in reader:
+        art = row["art"]
+        amount = int(row["amount"])
+        if row["year"] in {None, ""}:
+            start_stock[art] += amount
+        else:
+            key = get_key(row)
+            groups[key][art] += amount
+    records = tuple(VisRecord(vis=Inventory(vis), **dict(zip(key_fields, key))) for key, vis in groups.items())
+    return Inventory(start_stock), records
+
+
+_READERS: Final = {
+    "json": _read_json,
+    "csv": _read_csv,
+}
+
+
+@functools.total_ordering
+class Season(enum.Enum):
+    SPRING = "spring"
+    SUMMER = "summer"
+    AUTUMN = "autumn"
+    WINTER = "winter"
+
+    @functools.cached_property
+    def _index(self) -> int:
+        return next(index for index, member in enumerate(Season) if member is self)
+
+    def __lt__(self, other: "Season") -> bool:
+        if not isinstance(other, Season):
+            return NotImplemented
+        return self._index < other._index
+
+    @classmethod
+    def range(cls, start: "Season|None" = None, end: "Season | None" = None) -> "Iterable[Season]":
+        start = start or Season.SPRING
+        end = end or Season.WINTER
+        yield from (season for season in cls if start <= season <= end)
+
+
+class Inventory(Mapping[str, int]):
+    """Immutable mapping from elements to counts, supporting addition and subtraction"""
+
+    def __init__(self, obj: Mapping[str, int] | Iterable[str] | None = None, /, **kwargs: int):
+        self._data = dict()
+        # Copied from collections.Counter.update() to have the same semantics
+        if obj is not None:
+            if isinstance(obj, collections.abc.Mapping):
+                self._data.update(obj)
+            else:
+                for elem in obj:
+                    self._data[elem] = self._data.get(elem, 0) + 1
+        if kwargs:
+            for elem, count in kwargs.items():
+                self._data[elem] = self._data.get(elem, 0) + count
+        for k in list(self._data):  # we might del inside the loop, need to iterate on a copy
+            if self._data[k] == 0:
+                del self._data[k]
+
+    # collections.abc.Mapping minimal implementation. The inheritance will fill-in the Mapping protocol
+
+    def __getitem__(self, key: str) -> int:
+        return self._data.get(key, 0)
+
+    def __iter__(self):
+        return self._data.__iter__()
+
+    def __len__(self):
+        return self._data.__len__()
+
+    # Implement get() directly to allow for the default value as __getitem__ returns 0
+
+    @typing.overload
+    def get(self, key: str) -> int: ...
+    @typing.overload
+    def get[T](self, key: str, /, default: T) -> int | T: ...
+
+    def get(self, key: str, /, default=0):  # type: ignore
+        return self._data.get(key, default)
+
+    # Implement addition and subtraction
+
+    @classmethod
+    def _from_dict(cls, *args, **kwargs) -> "Inventory":
+        result = cls()
+        result._data = dict(*args, **kwargs)
+        return result
+
+    def __pos__(self) -> "Inventory":
+        return Inventory._from_dict((k, +v) for k, v in self.items() if v != 0)
+
+    def __neg__(self) -> "Inventory":
+        return Inventory._from_dict((k, -v) for k, v in self.items() if v != 0)
+
+    def __add__(self, other: "Inventory") -> "Inventory":
+        if not isinstance(other, Inventory):
+            return NotImplemented
+        return Inventory._from_dict(
+            (k, new_count) for k in self.keys() | other.keys() if (new_count := self[k] + other[k]) != 0
+        )
+
+    def __sub__(self, other: "Inventory") -> "Inventory":
+        if not isinstance(other, Inventory):
+            return NotImplemented
+        return Inventory._from_dict(
+            (k, new_count) for k in self.keys() | other.keys() if (new_count := self[k] - other[k]) != 0
+        )
+
+
+@dataclasses.dataclass(frozen=True)
+class VisRecord:
+    description: str
+    vis: Mapping[str, int]
+    year: int
+    season: Season
+    magus: str | None = None
+    period: int | None = None
+    end_year: int | None = None
+
+    def __post_init__(self) -> None:
+        """Simple-minded data validation"""
+        object.__setattr__(self, "year", int(self.year))
+        object.__setattr__(self, "season", Season(self.season))
+        object.__setattr__(self, "magus", self.magus if self.magus else None)
+        object.__setattr__(self, "period", int(self.period) if self.period else None)
+        object.__setattr__(self, "end_year", int(self.end_year) if self.end_year else None)
+
+    def is_active_on(self, year: int, season: Season) -> bool:
+        if self.period is None:
+            return self.year == year and self.season == season
+        else:
+            if self.season != season:
+                return False
+            if self.year > year:
+                return False
+            if self.end_year is not None and year > self.end_year:
+                return False
+            return ((year - self.year) % self.period) == 0
+
+
+@dataclasses.dataclass(frozen=True)
+class VisLedger:
+    """Description of vis stocks & consumption in time, and by magi."""
+
+    start: tuple[int, Season]
+    start_stock: Inventory
+    end: tuple[int, Season]
+    # year -> season -> record[]
+    ledger: Mapping[int, Mapping[Season, tuple[VisRecord, ...]]]
+    # year -> season -> stock
+    stocks: Mapping[int, Mapping[Season, Inventory]]
+    magi_uses: Mapping[str, Inventory]
+
+    @classmethod
+    def from_records(
+        cls,
+        records: Iterable[VisRecord],
+        *,
+        start_stock: Mapping[str, int] | None = None,
+        start_year: int | None = None,
+        start_season: Season | None = None,
+        end_year: int | None = None,
+        end_season: Season | None = None,
+    ) -> "VisLedger":
+        """Compute a full ledger from initial stocks and vis records"""
+        records = list(records)
+
+        start_year = start_year or min(record.year for record in records)
+        start_season = start_season or min(
+            (record.season for record in records if record.year == start_year), default=Season.SPRING
+        )
+
+        end_year = end_year or max(record.year for record in records)
+        end_season = end_season or max(
+            (record.season for record in records if record.year == end_year), default=Season.WINTER
+        )
+
+        stock = start_stock = Inventory(start_stock or {})
+
+        ledger: dict[int, Mapping[Season, tuple[VisRecord, ...]]] = {}
+        stocks: dict[int, Mapping[Season, Inventory]] = {}
+
+        for year in range(start_year, end_year + 1):
+            year_ledger: dict[Season, tuple[VisRecord, ...]] = {}
+            year_stocks: dict[Season, Inventory] = {}
+
+            start_season = start_season if year == start_year else Season.SPRING
+            end_season = end_season if year == end_year else Season.WINTER
+            for season in Season.range(start_season, end_season):
+                year_ledger[season] = tuple(record for record in records if record.is_active_on(year, season))
+                stock_update = sum((Inventory(record.vis) for record in year_ledger[season]), start=Inventory())
+                stock = stock + stock_update
+                year_stocks[season] = stock
+
+            ledger[year] = year_ledger
+            stocks[year] = year_stocks
+
+        magi = {record.magus for record in records if record.magus}
+        magi_uses = {
+            magus: sum(
+                (-Inventory(record.vis) for record in records if record.magus == magus),
+                start=Inventory(),
+            )
+            for magus in magi
+        }
+
+        return VisLedger(
+            start=(start_year, start_season),
+            start_stock=start_stock,
+            end=(end_year, end_season),
+            ledger=ledger,
+            stocks=stocks,
+            magi_uses=magi_uses,
+        )
+
+
+def _write_json(ledger: VisLedger, stream: TextIO) -> None:
+    json.dump(_to_json(ledger), stream, indent=2)
+
+
+def _to_json(obj: Any, path=tuple()) -> Any:
+    if isinstance(obj, (type(None), bool, int, float, str)):
+        return obj
+    elif isinstance(obj, (Sequence, set, frozenset)):
+        return [_to_json(element, (*path, index)) for index, element in enumerate(obj)]
+    elif isinstance(obj, Mapping):
+        return {_to_str(k): _to_json(v, (*path, k)) for k, v in obj.items()}
+    elif isinstance(obj, enum.Enum):
+        return _to_json(obj.value)
+    elif dataclasses.is_dataclass(obj):
+        return {
+            field.name: _to_json(value)
+            for field in dataclasses.fields(obj)
+            if (value := getattr(obj, field.name, dataclasses.MISSING)) is not dataclasses.MISSING
+        }
+    raise TypeError(f"Unsupported JSON conversion for {type(obj).__name__} object")
+
+
+def _to_str(obj) -> str:
+    if isinstance(obj, enum.Enum):
+        return _to_str(obj.value)
+    return str(obj)
+
+
+def _write_markdown(ledger: VisLedger, stream: TextIO) -> None:
+    _writelines(stream, _md_header())
+    _writelines(stream, _md_magi_uses(ledger))
+    _writelines(stream, _md_yearly_stocks(ledger))
+    _writelines(stream, _md_records(ledger))
+
+
+def _writelines(stream: TextIO, lines: Iterable[str], end: str = "\n") -> None:
+    """Wrapper around TextIO.writelines() that handles line ending"""
+    stream.writelines(f"{line}{end}" for line in lines)
+
+
+def _md_header() -> Iterable[str]:
+    return [
+        "# Vis",
+        "",
+        "**WARNING**: This file is automatically generated, do _not_ edit directly",
+    ]
+
+
+def _md_magi_uses(ledger: VisLedger) -> Iterable[str]:
+    yield ""
+    yield "## Magi consumption"
+    yield ""
+    vis_types = _sort_vis_names(set().union(*(uses.keys() for uses in ledger.magi_uses.values())))
+    magi = [*sorted(ledger.magi_uses)]
+    columns = {"": [magus.title() for magus in magi]}
+
+    for vis in vis_types:
+        columns[vis] = [str(ledger.magi_uses[magus].get(vis, "")) for magus in magi]
+    yield from _md_table(columns)
+
+
+def _md_yearly_stocks(ledger: VisLedger) -> Iterable[str]:
+    yield ""
+    yield "## Yearly stocks"
+    yield ""
+
+    vis_types_set = set(ledger.start_stock).union(
+        *(inventory.keys() for season_stocks in ledger.stocks.values() for inventory in season_stocks.values())
+    )
+    vis_types = _sort_vis_names(vis_types_set)
+    years = sorted(ledger.stocks, reverse=True)
+    columns = {"year": list(map(str, years))}
+    for vis in vis_types:
+        column: list[str] = []
+        for year in years:
+            year_stocks = ledger.stocks[year]
+            last_season = max(year_stocks)
+            column.append(str(year_stocks[last_season].get(vis, "")))
+        columns[vis] = column
+
+    if ledger.start_stock:
+        columns["year"].append("start")
+        for vis in vis_types:
+            columns[vis].append(str(ledger.start_stock.get(vis, "")))
+
+    yield from _md_table(columns)
+
+
+def _md_records(ledger: VisLedger) -> Iterable[str]:
+    yield ""
+    yield "## Movements"
+    yield ""
+    for year in sorted(ledger.ledger):
+        yield f"- **{year}**"
+
+        year_records = ledger.ledger[year]
+        for season in Season:
+            if season in year_records:
+                yield f"  - **{season.value.title()}**"
+
+                for record in year_records[season]:
+                    vis_diff = ", ".join(f"{amount:+} {vis.title()}" for vis, amount in record.vis.items())
+                    if record.magus:
+                        yield f"    - _{record.description}_ ({record.magus.title()}): {vis_diff}"
+                    else:
+                        yield f"    - _{record.description}_: {vis_diff}"
+
+
+_split_name = re.compile(r"[.,;:/|+&-]").split
+
+
+def _sort_vis_names(names: Iterable[str]) -> list[str]:
+    arts = ["cr", "in", "mu", "pe", "re", "an", "aq", "au", "co", "he", "ig", "im", "me", "te", "vi"]
+    indices = {art: index for index, art in enumerate(arts)}
+
+    def key(name: str) -> tuple[int, ...]:
+        parts = [part.strip().lower() for part in _split_name(name)]
+        return (len(parts), *tuple(indices.get(part.lower()[:2], len(part)) for part in parts))
+
+    return sorted([*arts, *names], key=key)
+
+
+def _md_table(columns: dict[str, Any]) -> Iterable[str]:
+    headers = tuple(columns)
+    widths = [max(3, len(col), max(map(len, columns[col]))) for col in headers]
+    # first column is right aligned, then everything is centered
+    alignments = ">" + "^" * (len(headers) - 1)
+
+    def _row(cells: Iterable[str]) -> str:
+        cells = [f" {row: {align}{width}} " for row, align, width in zip(cells, alignments, widths, strict=True)]
+        return "|" + "|".join(cells) + "|"
+
+    yield _row(header.title() for header in headers)
+    yield _md_alignment_row(alignments, widths)
+    rows: Iterable[tuple[str, ...]] = zip(*(columns[col] for col in headers))
+    yield from (_row(row) for row in rows)
+
+
+def _md_alignment_row(alignment: str, widths: Iterable[int]) -> str:
+    fstring_to_md_align = {">": _md_table_align_right, "^": _md_table_align_center}
+    cells = (fstring_to_md_align[align](width) for align, width in zip(alignment, widths, strict=True))
+    return "|" + "|".join(cells) + "|"
+
+
+def _md_table_align_right(width: int) -> str:
+    return " " + "-" * (width - 1) + ": "
+
+
+def _md_table_align_center(width: int) -> str:
+    return " :" + ("-" * max(0, width - 2)) + ": "
+
+
+_WRITERS: Final = {
+    "json": _write_json,
+    "md": _write_markdown,
+}
+
+if __name__ == "__main__":
+    cli()
